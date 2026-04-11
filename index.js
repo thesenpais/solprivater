844f4fd629b483aa0424062cd0c0457fbaab1c45
codybdavis6 <codybdavis6@gmail.com>
2026-04-11 11:50:32 +0100

    new update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors({
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+    allowedHeaders: ['*']
 }));
 
 // Additional CORS headers for preflight requests
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', '*');
 
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
